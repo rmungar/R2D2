5427cccbae8152d8ab97a89d8b339d372f627f37
rmungar <mgraul@iesrafaelalberti.es>
2024-01-11 19:48:33 +0100

    |

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,13 @@
 
 
 fun main(){
-    r2d2()
+    r2d2_1() // FUNCIONA CON VALORES PREDEFINIDOS
+    r2d2_2() // FUNCIONA CON VALORES INGRESADOS POR EL USUARIO
 }
 /*
  * Esta función solo le va a pasar los parametros a la función que mueve el robot
  */
-fun r2d2() {
+fun r2d2_1() {
     mover(10, 5, -2)
     mover(0, 0, 0)
     mover()
@@ -55,4 +56,52 @@ fun mover(vararg movs: Int) {
             println("X: $posx, Y: $posy, direction: POSITIVE X")
         }
     }
+}
+
+fun r2d2_2(){
+    print("Ingrese una lista de movimiento con el siguiente formato (5, -9, 25) -> ")
+    val movs = readln().split(", ")
+    var posx = 0
+    var posy = 0
+    var direccion = 1
+    try {
+        for (movimiento in movs){
+            when (direccion) {
+                1 -> {
+                    posy += movimiento.toInt()
+                    direccion = 2
+                }
+                2 -> {
+                    posx -= movimiento.toInt()
+                    direccion = 3
+                }
+                3 -> {
+                    posy -= movimiento.toInt()
+                    direccion = 4
+                }
+                4 -> {
+                    posx += movimiento.toInt()
+                    direccion = 1
+                }
+            }
+        }
+        when (direccion) {
+            1 -> {
+                println("X: $posx, Y: $posy, direction: POSITIVE Y")
+            }
+            2 -> {
+                println("X: $posx, Y: $posy, direction: NEGATIVE X")
+            }
+            3 -> {
+                println("X: $posx, Y: $posy, direction: NEGATIVE Y")
+            }
+            4 -> {
+                println("X: $posx, Y: $posy, direction: POSITIVE X")
+            }
+        }
+    }
+    catch (_:NumberFormatException){
+        println("X: $posx, Y: $posy, direction: POSITIVE Y")
+    }
+
 }
